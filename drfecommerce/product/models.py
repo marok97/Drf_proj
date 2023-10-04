1d9b1fdd3bdeddd73c710223ec8b4ac66135ceeb
chran <achristoffer@ymail.com>
2023-10-05 01:25:25 +0200

    Add first models and serializers + first migration

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -1,37 +1,69 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here
 
 
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Product(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField(max_length=200)
-    type = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=200, blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey(
+        "Category", null=True, blank=True, on_delete=models.SET_NULL
+     )
+
+    def __str__(self) -> str:
+        return self.name
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=30)
-    product = models.ManyToManyField(Product)
+# class ProductLine(models.Model):
+#     price = models.DecimalField()
+#     sku = models.CharField()
+#     stock_quantity = models.IntegerField()
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
-class ProductLine(models.Model):
-    price = models.DecimalField()
-    sku = models.CharField()
-    stock_quantity = models.IntegerField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+# class ProductImage(models.Model):
+#     name = models.CharField()
+#     alternative_text = models.CharField()
+#     url = models.ImageField()
+#     product_line = models.ForeignKey(ProductLine)
+
+#     def __str__(self) -> str:
+#         return self.name
 
 
-class ProductImage(models.Model):
-    name = models.CharField()
-    alternative_text = models.CharField()
-    url = models.ImageField()
-    product_line = models.ForeignKey(ProductLine)
+# class Attribute(models.Model):
+#     name = models.CharField()
+
+#     def __str__(self) -> str:
+#         return self.name
 
 
-class Attribute(models.Model):
-    name = models.CharField()
-
-
-class AttributeValue(models.Model):
-    value = models.CharField()
-    attribute = models.ForeignKey(Attribute)
+# class AttributeValue(models.Model):
+#     value = models.CharField()
+#     attribute = models.ForeignKey(Attribute)
--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -28,58 +28,55 @@ load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
- 
+
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    #External Package
-    'rest_framework',
-    #Internal Apps
-    'products'
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # External Package
+    "rest_framework",
+    # Internal Apps
+    "drfecommerce.product",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.c ontrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'drfecommerce.urls'
+ROOT_URLCONF = "drfecommerce.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'drfecommerce.wsgi.application'
+WSGI_APPLICATION = "drfecommerce.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-
 
 
 # Password validation
@@ -87,16 +84,16 @@ WSGI_APPLICATION = 'drfecommerce.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -104,9 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -116,11 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {}
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
